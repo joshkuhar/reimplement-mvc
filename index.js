1bcf21144ba29631b35ab8282eeb27fe747aa220
Josh Kuhar <jdkuhar@gmail.com>
2016-08-04 10:30:43 -0400

    update

--- index.js
+++ index.js
@@ -1,184 +1,94 @@
-var QUESTIONS = [
-    {
-        text: '<:48:x<:65:=<:6C:$=$=$$~<:03:+$~<:ffffffffffffffbd:+$<:ffffffffffffffb1:+$<:57:~$~<:18:x+$~<:03:+$~<:06:x-$x<:0e:x-$=x<:43:x-$',
-        answers: [
-            '0815',
-            '2B',
-            'BAM128',
-            'Barely'
-        ],
-        correct: 0
-    },
-    {
-        text: '+0+0+0+0+0+0+0+2)+0+0+9)+7))+3)-0-0-0-0-0-0-0-9)+0+0+0+0+0+0+0+0+7)-8)+3)-6)-8)-7-0-0-0-0-0-0)',
-        answers: [
-            '0815',
-            '2B',
-            'BAM128',
-            'Barely'
-        ],
-        correct: 1
-    },
-    {
-        text: '*6*3p*4*3*2*0p*2*1*0pp>0*1*0p*5*4*0p*5*4*2*1*0p*4*3p*1*0p/+0p+0*6*5*2p+0*5*0p',
-        answers: [
-            '0815',
-            '2B',
-            'BAM128',
-            'Barely'
-        ],
-        correct: 2
-    },
-    {
-        text: ']xhhhhooooooooohhhhhhxooooooooxooooooxjjjxhoooohhhxhohhhhhhhxhhhhjjjhhhxhhhhooooooooohhhhhhxjjjxxjjjjjjjxjhhhhxjhhhhhhhhjjjhh~',
-        answers: [
-            '0815',
-            '2B',
-            'BAM128',
-            'Barely'
-        ],
-        correct: 3
-    }
-];
-
-//container for questions
-var questionsPageElement = $('.questions-page');
-
-//question tracker
-var questionCurrentElement = $('.question-current');
-var questionsTotalElement = $('.questions-total');
-
-//question
-var questionElement = $('.question');
-
-//answer
-var answersElement = $('.answers');
-
-//results of quiz and restart button
-var resultsPageElement = $('.results-page');
-var scoreElement = $('.score');
-var restartButtonElement = $('.restart-button');
-
-//end of game and restart function
-var showResults = function() {
-    questionsPageElement.hide();
-    resultsPageElement.show();
-};
-//start new game function
-var showQuestions = function() {
-    resultsPageElement.hide();
-    questionsPageElement.show();
-};
-//resests score
-var resetScore = function() {
-    scoreElement.text(0);
-};
-//increases score by 1
-var increaseScore = function() {
-    var score = parseInt(scoreElement.text(), 10);
-    scoreElement.text(score + 1);
-};
-//sets question
-var setQuestion = function(questionIndex) {
-    var question = QUESTIONS[questionIndex];
-    questionCurrentElement.text(questionIndex);
-    questionElement.text(question.text);
-    answersElement.empty();
-    for (var i=0; i<question.answers.length; i++) {
-        var answer = question.answers[i];
-        answersElement.append('<li><button type="button">' + answer + '</button></li>');
-    }
-};
-//captures answer and determines if answer is correct
-answersElement.on('click', 'button', function() {
-    var choice = $(this).parent().index();
-    var questionIndex = parseInt(questionCurrentElement.text(), 10);
-    var question = QUESTIONS[questionIndex];
-    if (question.correct === choice) {
-        increaseScore();
-    }
-
-    if (questionIndex + 1 < QUESTIONS.length) {
-        setQuestion(questionIndex + 1);
-    }
-    else {
-        showResults();
-    }
-});
-//listens for restart button being clicked
-restartButtonElement.click(function() {
-    setQuestion(0);
-    resetScore();
-    showQuestions();
-});
-//initial page load
-$(document).ready(function() {
-    questionsTotalElement.text(QUESTIONS.length);
-    setQuestion(0);
-});
-
-/*
+//MODEL
 var Model = function(){
-  this.questions = [["Question 1", "a"], ["Questions 2", "b"], ["Question 3", "c" ]];
+  this.questions = [["<:48:x<:65:=<:6C:$=$=$$~<:03:+$~<:ffffffffffffffbd:+$<:ffffffffffffffb1:+$<:57:~$~<:18:x+$~<:03:+$~<:06:x-$x<:0e:x-$=x<:43:x-$", 
+                     "0815"], 
+                    ["+0+0+0+0+0+0+0+2)+0+0+9)+7))+3)-0-0-0-0-0-0-0-9)+0+0+0+0+0+0+0+0+7)-8)+3)-6)-8)-7-0-0-0-0-0-0)", 
+                    "2B"], 
+                    ["*6*3p*4*3*2*0p*2*1*0pp>0*1*0p*5*4*0p*5*4*2*1*0p*4*3p*1*0p/+0p+0*6*5*2p+0*5*0p", 
+                    "BAM128" ], 
+                    ["]xhhhhooooooooohhhhhhxooooooooxooooooxjjjxhoooohhhxhohhhhhhhxhhhhjjjhhhxhhhhooooooooohhhhhhxjjjxxjjjjjjjxjhhhhxjhhhhhhhhjjjhh~", 
+                    "Barely"]
+                    ];
   this.answers = ['0815', '2B', 'BAM128', 'Barely'];
-  this.testQuestion = null;
+  this.questionNumber = 0;
+  this.numberCorrect = 0;
 };
-Model.prototype.getQuestion = function(questionNumber){
-  this.question = this.questions[questionNumber][0];
-  console.log(this.question);
-  console.log("Choices: " + this.answers);
-};
-Model.prototype.showAnswers = function(){
-  console.log(this.answers);
-};
-Model.prototype.checkAnswer = function(answer, questionNumber){
-  this.correct = this.questions[questionNumber][1];
-  if(answer === this.correct){
-    this.testQuestion = this.correct;
-  }
-};
-/*Model.prototype.score = function(){
-  this.score = score;
-};*//*
-var questionNumber = 1;
-var quiz = new Model();
 
+//VIEW
 var View = function(){};
 
-View.prototype.question = function(element, test){
-  $(element).empty().append(test);
-  //$(element).append(test);
+View.prototype.question = function(element, input){
+  $(element).empty().append(input);
 };
-
-View.prototype.questionNumber = function(){
-  $('.questions-total').text(questions.length);
-  $('.question-current').text(questionNumber);
-  //console.log(questionNumber);
-};
-
-View.prototype.displayAnswers = function(element, answers){
-  for(var answer in answers){
-    $(element).append('<li><button type="button">' + answers[answer] + '</button></li>');
+View.prototype.answers = function(answers){
+   for(var answer in answers){
+   $('ul').append('<li><button type="button" id="' + answers[answer] + '">' + answers[answer] + '</button></li>');
   }
 };
-
-View.prototype.displayResults = function(){
+View.prototype.results = function(numberCorrect, questionNumber){
+  $('.score').text(numberCorrect);
+  $('.questions-total').text(questionNumber);
+};
+View.prototype.showResults = function(){
   $('.questions-page').hide();
   $('.results-page').show();
-  $('.score').text("number correct");
 };
 
-questions = ['a', 'b', 'c'];
-answers = ['0815', '2B', 'BAM128', 'Barely'];
+//CONTROLLER
+var Controller = function(model, view){
+  this.questionNumber = model.questionNumber;
+};
+Controller.prototype.getQuestion = function(){
+  model.question = model.questions[this.questionNumber][0];
+  view.question('.question', model.question);
+};
+Controller.prototype.displayAnswers = function(answers){
+  view.answers(model.answers);
+};
+Controller.prototype.checkAnswers = function(){
+  $('.answers').on('click', 'button', function(event){
+    event.preventDefault();
+    if ($(this).attr('id') === model.questions[controller.questionNumber][1]){
+      controller.questionNumber++;
+      model.questionNumber++;
+      model.numberCorrect++;
+      if (controller.questionNumber < 4){
+        controller.getQuestion();
+      } else {
+        view.results(model.numberCorrect, model.questionNumber);
+        view.showResults();
+      }
+    } else if ($(this).attr('id') !== model.questions[controller.questionNumber][1]){
+      controller.questionNumber++;
+      model.questionNumber++;
+      if (controller.questionNumber < 4){
+        controller.getQuestion();
+      } else {
+        view.results(model.numberCorrect, model.questionNumber);
+        view.showResults();
+      }
+    }
+  })
+};
+Controller.prototype.restart = function(){
+  $('.restart-button').click(function(event){
+    event.preventDefault();
+    $('.questions-page').show();
+    $('.results-page').hide();
+    model.questionNumber = 0;
+    model.numberCorrect = 0;
+    controller.questionNumber = 0;
+  });
+};
+
+var model = new Model();
 var view = new View();
+var controller = new Controller(model, view);
 
-view.displayAnswers('.answers', answers);
-
-view.questionNumber();
-
-
-
-
+controller.getQuestion();
+controller.displayAnswers(model.answers);
+controller.checkAnswers();
+controller.restart();
 
 
 
@@ -186,10 +96,4 @@ view.questionNumber();
 
 
 
-
-
-
-
-
-*/
 
